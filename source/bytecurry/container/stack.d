a3ffac39c2dd085d6e07d788709bc12944cdd8d2
Thayne McCombs <astrothayne@gmail.com>
2016-05-30 22:27:19 -0600

    Refactor stack interface.

--- source/bytecurry/container/stack.d
+++ source/bytecurry/container/stack.d
@@ -1,150 +1,231 @@
 module bytecurry.container.stack;
 
+import std.container.util : make;
 import std.container.slist;
-import std.range : isInputRange;
+import std.range;
 import std.traits : isImplicitlyConvertible;
+
+/**
+ * Minimal interface for a stack.
+ */
+interface Stack(E) {
+    /**
+     * Check if the stack is empty.
+     */
+    bool empty() @property;
+
+    /**
+     * Remove the front element from the stack
+     */
+    void removeFront();
+    /// ditto
+    alias stableRemoveFront = popFront;
+
+    /**
+     * Get the top element of the stack;
+     */
+    E front() @property;
+
+    /**
+     * Like `front` but moves the
+     * front value rather than copying.
+     */
+    E moveFront();
+
+    /**
+     * Insert an element at the end of the stack.
+     */
+    size_t insertFront(E element);
+    /// ditto
+    alias stableInsertFront = insertFront;
+    /// ditto
+    alias insert = insertFront;
+    /// ditto
+    alias stableInsert = insertFront;
+    /// ditto
+    alias linearInsert = insertFront;
+}
+
+/**
+ * Test if a type is a stack. That is,
+ * you can test if it is empty, remove the front element,
+ * look at the front element, and insert an item in the front.
+ * I.e. it defines `empty`, `front`, `removeFront`, and `insertFront`
+ */
+template isStack(C) {
+    enum isStack = is(typeof((inout int = 0) {
+            C c = C.init;
+            if (c.empty) {} // can test for empty
+            c.removeFront(); // can remove front
+            auto h = c.front; // can get front
+            c.insertFront(h); // can insert at font
+            }));
+}
+
+/**
+ * A richer interface for a stack.
+ * It makes a stack a forward range and an output range, and provides
+ * default implementations for several methods.
+ */
+abstract class RichStack(E) : Stack!E, ForwardRange!E, OutputRange!E {
+    // new methods
+
+    /**
+     * Clear all elements from the stack.
+     */
+    abstract void clear();
+
+    /**
+     * Remove the top element of the stack and return it.
+     */
+    E removeAny() {
+        auto ret = moveFront();
+        removeFront();
+        return ret;
+    }
+
+
+    void popFront() {
+        removeFront();
+    }
+
+    void put(E element) {
+        insertFront(element);
+    }
+
+    /**
+     * Default implementation of opSlice.
+     * It calls empty, front, and removeFront.
+     *
+     * Implementations may want to override to avoid
+     * virtual function calls.
+     */
+    int opApply(int delegate(E) dg) {
+        int res;
+        while(!empty) {
+            res = dg(front);
+            if (res) {
+                return res;
+            }
+            removeFront();
+        }
+        return 0;
+    }
+
+    /// ditto
+    int opApply(int delegate(size_t, E) dg) {
+        int res;
+        size_t i;
+        while(!empty) {
+            res = dg(i, front);
+            if (res) {
+                return res;
+            }
+            i++;
+            removeFront();
+        }
+        return 0;
+    }
+}
+
+/**
+ * Class that wraps another container that acts like a stack.
+ */
+template ContainerStack(C) if (isStack!C && isForwardRange!(typeof(C.init.opSlice()))) {
+    private alias E = ElementType!C;
+    static if (is(C : Stack!E)) {
+        alias ContainerStack = C;
+    } else {
+        class ContainerStack: RichStack!E {
+            protected C _container;
+            this(C cont) {
+                _container = cont;
+            }
+
+            this() {
+                _container = make!C();
+            }
+
+            this(E[] elements...) {
+                _container = make!C(elements);
+            }
+
+            this(Stuff)(Stuff stuff) if (isInputRange!Stuff && is(ElementType!Stuff: E)) {
+                _container = make!C(stuff);
+            }
+
+            bool empty() @property {
+                return _container.empty;
+            }
+
+            void removeFront() {
+                _container.removeFront();
+            }
+
+            E front() @property {
+                return _container.front;
+            }
+
+            E moveFront() {
+                return .moveFront(_container);
+            }
+
+            size_t insertFront(E element) {
+                return _container.insertFront(element);
+            }
+
+            override void clear() {
+                static if (is(typeof(_container.clear()))) {
+                    _container.clear();
+                } else {
+                    _container = C.init;
+                }
+            }
+
+            ForwardRange!E save() {
+                return inputRangeObject(_container.opSlice());
+            }
+
+            static if (is(typeof(_container.dup))) {
+                /**
+                 * Duplicate the stack
+                 */
+                ContainerStack dup() @property {
+                    return new ContainerStack(_container.dup);
+                }
+            }
+
+            override bool opEquals(Object o) {
+                auto rhs = cast(ContainerStack) o;
+                if (rhs) {
+                    return _container == rhs._container;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+}
+
+
 
 /**
  * A simple stack interface that wraps an SList.
  *
  */
-struct Stack(T) {
+alias SListStack(E) = ContainerStack!(SList!E);
 
-    /**
-     * Create a Stack from an underlying SList.
-     * Note that the slist and stack share data.
-     */
-    this(SList!T slist) pure {
-        list = slist;
-    }
-
-    /**
-     * Construct, initialized with a number of values.
-     * The first value is at the top of the stack.
-     */
-    this(U)(U[] values...) pure if (isImplicitlyConvertible!(U, T)) {
-        list = Slist!T(values);
-    }
-
-    /**
-     * Construct, initialized with values from an input range.
-     * The first value of the range is at the top of the stack.
-     */
-    this(Stuff)(Stuff stuff) pure
-    if (isInputRange!Stuff && isImplicitlyConvertible!(ElementType!Stuff, T) && !is(Stuff == T[])) {
-        list = Slist!T(stuff);
-    }
-
-    /**
-     * Property returning `true` if and only if the stack is empty.
-     *
-     * Complexity: $(BIGOH 1)
-     */
-    @property bool empty() const pure {
-        return list.empty;
-    }
-
-    /**
-     * Property returning the top element in the stack.
-     */
-    @property ref T front() pure {
-        return list.front;
-    }
-
-     /**
-      * Return a range of the underlying SList
-      */
-    auto save() pure {
-        return list[];
-    }
-    alias opSlice = save;
-
-    /**
-     * Remove the top element from the stack.
-     *
-     * Complexity: $(BIGOH 1)
-     */
-    void popFront() pure {
-        list.removeFront();
-    }
-    /// ditto
-    alias removeFront = popFront;
-    alias stableRemoveFront = popFront;
-
-    /**
-     * Remove the top element from the stack and return it.
-     *
-     * Complexity: $(BIGOH 1)
-     */
-    T removeAny() pure {
-        return list.removeAny();
-    }
-    alias stableRemoveAny = removeAny;
-
-     /**
-      * Push one or more items onto the top of the stack. If more than
-      * one item is pushed, the first item is on top.
-      *
-      * Complexity: $(BIGOH m) where `m` is the length of `stuff`
-      */
-    void put(T item) pure {
-        list.insertFront(item);
-    }
-
-     /// ditto
-    void put(Stuff)(Stuff stuff) pure
-    if (isInputRange!Stuff && isImplicitlyConvertible(ElementType!Stuff, T)) {
-         list.insertFront(stuff);
-    }
-
-    /// ditto
-    alias insert = put;
-    /// ditto
-    alias stableInsert = put;
-    /// ditto
-    alias insertFront = put;
-    /// ditto
-    alias stableInsertFront = put;
-
-    /**
-     * Comparison for equality.
-     *
-     * Complexity: $(BIGOH min(n, m)) where `m` is the number of elements in `rhs`
-     */
-    bool opEquals(const Stack rhs) pure const {
-        return opEquals(rhs);
-    }
-
-    /// ditto
-    bool opEquals(const ref Stack rhs) pure const {
-        return list == rhs.list;
-    }
-
-    /**
-     * Duplicate the stack.
-     *
-     * Complexity: $(BIGOH n)
-     */
-    Stack dup() pure {
-        return Stack(list.dup);
-    }
-
-    /**
-     * Remove all contents of the stack.
-     *
-     * Complexity: $(BIGOH 1)
-     */
-    void clear() pure {
-        list.clear();
-    }
-
-
-    private SList!T list;
+/**
+ * Create a new stack initialized with zero or more
+ * elements.
+ */
+RichStack!E stack(E)(E[] elements...) {
+    return new SListStack!E(elements);
 }
 
 ///
 unittest {
-    Stack!int stack;
+    RichStack!int stack = stack!int();
 
     stack.put(1);
     stack.put(5);
@@ -153,5 +234,13 @@ unittest {
     stack.popFront();
     assert(stack.removeAny() == 5);
     assert(stack.removeAny() == 1);
+    assert(stack.empty);
+}
+
+unittest {
+    auto stack = stack!int();
+    stack.put(1);
+    stack.insertFront(2);
+    stack.clear();
     assert(stack.empty);
 }
